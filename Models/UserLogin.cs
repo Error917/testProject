becb1397108dc3bd574d14713d3fe6d962366544
Pantho Sarkar <124734858+Error917@users.noreply.github.com>
2024-01-02 03:32:31 +0600

    Add project files.

---
+++ Models/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿namespace testProject.Models
+{
+    public class UserLogin
+    {
+        public int Id  { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int PhoneNo { get; set; }
+    }
+}
